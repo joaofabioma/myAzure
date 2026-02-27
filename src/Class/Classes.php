e060de2cd900c508b2419fbfb1f062389bc76361
JoaoFabioMA <amarall.jf@gmail.com>
2026-02-27 15:08:37 -0400

    Classes

---
+++ src/Class/Classes.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Class;
+
+
+class Classes
+{
+    public static function getClasses(): array
+    {
+        return [
+            //HTML::class,
+            //Security::class,
+            //PerformanceMonitor::class,
+        ];
+    }
+}
