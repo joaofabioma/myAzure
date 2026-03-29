43f7fd90ecdb9056317a82b8e729a3be35737c97
JoaoFabioMA <amarall.jf@gmail.com>
2026-03-28 21:45:27 -0400

    vars

--- inc/prepend.php
+++ inc/prepend.php
@@ -16,6 +16,7 @@ $env['PROJECTS'] =  array_map('trim', explode(',', $env['PROJECTS'])) ?? [];
 !defined('PROJETO') && define('PROJETO', $env['PROJECT_DEFAULT']);
 !defined('EDEV') && define('EDEV', $env['EMAIL_DEV']);
 !defined('TIMER') && define('TIMER', $env['TEMPO_RECARREGAR_PAGINA_MINUTOS']);
-!defined('ONLINE') && define('ONLINE', TRUE);
-!defined('DEBUG') && define('DEBUG', FALSE); // colocar no .env
+!defined('ONLINE') && define('ONLINE',(bool) $env['REQUEST_ONLINE']);
+!defined('DEBUG') && define('DEBUG', (bool) $env['APP_DEBUG']);
 !defined('UACESS') && define('UACESS', $env['URL_ACCESS']); 
+!defined('LISTTASKS') && define('LISTTASKS', $env['URL_LIST_TASKS']); 
